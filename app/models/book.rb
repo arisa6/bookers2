d2318d118ef67351edb338815c9b5918093a36b5
EC2 Default User <ec2-user@ip-172-31-46-196.ap-northeast-1.compute.internal>
2022-07-07 06:18:27 +0000

    検索機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,4 +11,22 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+  
+  
+  # 検索方法分岐
+  def self.looks(search, word)
+    if search == "perfect_match"  #完全一致
+      @book = Book.where("title LIKE?","#{word}")
+    elsif search == "forward_match"  #前方一致
+      @book = Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"  #後方一致
+      @book = Book.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"  #部分一致
+      @book = Book.where("title LIKE?","%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
+  
+  #whereメソッドを使いデータベースから該当データを取得し、変数に代入する
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'searches/search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root :to =>"homes#top"
@@ -13,6 +14,11 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+  
+  get "search" => "searches#search"
+  # 検索ボタンが押された時、Searchesコントローラーのsearchアクションが実行されるように定義
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
